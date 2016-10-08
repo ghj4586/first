0ef74966007358acab4ca0e83d94530f3e880db8
ghj4586 <ghj4586@163.com>
2016-10-08 23:01:29 +0800

    dfffff

--- src/com/atguigu/HelloWorld.java
+++ src/com/atguigu/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		
-		
+		System.out.println("ggggg");
 		System.out.println("hello world");
 		System.out.println("abc");
 		System.out.println("111111");
