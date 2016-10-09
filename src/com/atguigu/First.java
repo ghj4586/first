8ed535cd98d5a0e807e58c2eb00c4975801aaee3
ghj4586 <ghj4586@163.com>
2016-10-09 18:56:48 +0800

    sss

---
+++ src/com/atguigu/First.java
@@ -0,0 +1,9 @@
+package com.atguigu;
+
+public class First {
+
+	
+	private int num;
+	private String ss;
+	
+}
--- src/com/atguigu/HelloWorld.java
+++ src/com/atguigu/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		
+		System.out.println("sssss");
 		System.out.println("ggggg");
 		System.out.println("hello world");
 		System.out.println("abc");
